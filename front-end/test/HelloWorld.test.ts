c987fc769cf0191bf0261e61068f8b2437040934
keunhyeok <hyc7575@naver.com>
2018-02-13 18:28:13 +0900

    주석

--- front-end/test/HelloWorld.test.ts
+++ front-end/test/HelloWorld.test.ts
@@ -7,8 +7,10 @@ describe('HelloWorld.vue', () => {
     it('should initialize correctly', () => {
         const wrapper = mount(HelloWorld);
         
+        expect(wrapper.vm.msg).toEqual('Hello!!'); // msg data값과 일치하는지 테스트
+
         renderer.renderToString(wrapper.vm, (err, str) => {
-            expect(str).toMatchSnapshot();
+            expect(str).toMatchSnapshot(); // 테스트 내에서 server랜더링 된 값을 string화 하여 snapShot 저장
         });
     })
 });
